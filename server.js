9e874d1d5d0950199fcff7b5f7b18d84443472d4
Aravind <aravind08222@gmail.com>
2021-02-22 22:13:43 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Api running");
+});
+
+app.listen(PORT, () => {
+    console.log(`-----> Server Started on port ${PORT}`);
+});
