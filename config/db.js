5012dc3f8235c2c0e3dd4992a7bdec784c1be9fe
Aravind <aravind08222@gmail.com>
2021-02-23 11:49:23 +0530

    Implemented user registered and gravitar avatar

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
         //await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
-        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
         console.log("-----> MongoDb Database Connected");    
     } catch(err) {
         console.log(err.message);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {check, validationResult} = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route       POST api/users
 // @desc        Register User
@@ -13,15 +17,56 @@ router.post("/" , [
         .isEmail(),
     check('password', 'Please enter a password with minmum 6 or more characters')
         .isLength({ min: 6 })
-], (req, res) => {
-    const errors = validationResult(req)
-    console.log(errors)
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            errors: errors.array()
-        });
+    ], async (req, res) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {name, email, password} = req.body;
+
+        try {
+            //See if user exits
+            let user = await User.findOne({ email });
+
+            if(user){
+                return res.status(400).json({
+                    errors: [{
+                        msg: "User Already Exists"
+                    }]
+                });
+            }
+
+            //Get users Gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'identicon' 
+                
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            //Encrypt Password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            
+            await user.save();
+
+            //Return JWT
+            res.send('User Registerd');
+        } catch(err){
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     }
-    res.send('asd')
-});
+);
 
 module.exports = router;
