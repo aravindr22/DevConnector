de72173ee1e3472e000ac3d50a605c8cfdc57dbf
Aravind <aravind08222@gmail.com>
2021-03-01 13:13:25 +0530

    Added user delete and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,27 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route       DELETE api/profile
+// @desv        Delete profile, user & posts
+// @access      Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        //@todo- remove users posts
+        //Remove Profile
+        console.log(req.user_id)
+        await Profile.findOneAndRemove({user: req.user.id});
+        //Remove User
+        await User.findOneAndRemove({_id: req.user.id});
+        res.json({
+            msg: 'User Deleted'
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({
+            msg: error.message
+        });
+    }
+});
+
+
 module.exports = router;
