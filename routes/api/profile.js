79911c10b5c466fb864ee22a1367054ee26125e7
Aravind <aravind08222@gmail.com>
2021-03-01 20:39:47 +0530

    Profile add experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -156,7 +156,6 @@ router.delete('/', auth, async (req, res) => {
     try {
         //@todo- remove users posts
         //Remove Profile
-        console.log(req.user_id)
         await Profile.findOneAndRemove({user: req.user.id});
         //Remove User
         await User.findOneAndRemove({_id: req.user.id});
@@ -169,6 +168,62 @@ router.delete('/', auth, async (req, res) => {
             msg: error.message
         });
     }
+});
+
+// @route       PUT api/profile/experience
+// @desv        Add profile experience
+// @access      Private
+router.put('/experience', [auth, [
+    check('title', "Title is required")
+        .not()
+        .isEmpty(),
+    check('company', "Company is required")
+        .not()
+        .isEmpty(),
+    check('from', "From date is required")
+        .not()
+        .isEmpty()
+        
+] ], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newWxep = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newWxep);
+        await profile.save();
+
+        res.send(profile);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+
 });
 
 
