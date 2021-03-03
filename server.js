fe37622c63f7463a06921124c7ef73a66a4c166a
Aravind <aravind08222@gmail.com>
2021-03-03 22:16:25 +0530

    Added post route for new post

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express'),
 const usersRoutes = require('./routes/api/users'),
     authRoutes = require('./routes/api/auth'),
     profileRoutes = require('./routes/api/profile'),
-    postsRoutes = require('./routes/api/profile');
+    postsRoutes = require('./routes/api/posts');
 
 const PORT = process.env.PORT || 5000;
 
