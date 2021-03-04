db665ee6ba76ba6a258aedfe2c92567f2e1df594
Aravind <aravind08222@gmail.com>
2021-03-04 20:35:47 +0530

    Add comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -177,4 +177,48 @@ router.put("/unlike/:id", auth, async (req, res) => {
     }
 });
 
+// @route       POST api/posts/comment/:id
+// @desv        comment on a Post
+// @access      Private
+router.post("/comment/:id" , [auth, [
+    check('text', "Text is required")
+        .not()
+        .isEmpty()
+] ], async (req, res) => {
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+        await post.save();
+
+        res.json(post);
+        
+    } catch (err) {
+        console.error(err.message);
+        if(error.kind === 'ObjectId'){
+            return res.status(404).json({
+                msg: 'Post not found'
+            });
+        }
+        res.status(500).send('Server Error');
+    }
+
+});
+
 module.exports = router;
