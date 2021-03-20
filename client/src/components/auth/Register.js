0d4feca1c149743aa52f64a57660433905ab3ade
Aravind <aravind08222@gmail.com>
2021-03-20 23:12:12 +0530

    Added alert.js in layout

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
+import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -17,7 +20,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
-            console.log("Pass Not match")
+            setAlert("Pass Not match", 'danger')
         } else {
             console.log(formData);
         }
@@ -81,4 +84,8 @@ const Register = () => {
     );
 }
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+}
+
+export default connect(null, {setAlert})(Register);
