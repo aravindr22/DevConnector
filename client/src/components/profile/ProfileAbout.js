0d9dadd2eea7c931723783078f144a0b8cf1bbdb
Aravind <aravind08222@gmail.com>
2021-04-22 00:04:38 +0530

    profile about completed

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -9,20 +9,20 @@ const ProfileAbout = ({ profile: {
     return (
         <Fragment>
             <div class="profile-about bg-light p-2">
-                <h2 class="text-primary">John's Bio</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
-                    doloremque nesciunt, repellendus nostrum deleniti recusandae nobis
-                    neque modi perspiciatis similique?
-                </p>
-                <div class="line"></div>
+                {bio && (
+                    <Fragment>
+                        <h2 class="text-primary">{name.trim().split(" ")[0]}'s Bio</h2>
+                        <p>
+                            {bio}
+                        </p>
+                        <div class="line"></div>
+                    </Fragment>
+                )}
                 <h2 class="text-primary">Skill Set</h2>
                 <div class="skills">
-                    <div class="p-1"><i class="fa fa-check"></i> HTML</div>
-                    <div class="p-1"><i class="fa fa-check"></i> CSS</div>
-                    <div class="p-1"><i class="fa fa-check"></i> JavaScript</div>
-                    <div class="p-1"><i class="fa fa-check"></i> Python</div>
-                    <div class="p-1"><i class="fa fa-check"></i> C#</div>
+                    {skills && skills.map((skill, index) => (
+                        <div key={index} class="p-1"><i class="fa fa-check"></i>{skill}</div>
+                    ))}
                 </div>
             </div>
         </Fragment>
