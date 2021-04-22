033e012b6dadf34dfad097dae5567ab28736c973
Aravind <aravind08222@gmail.com>
2021-04-23 00:08:32 +0530

    Completed profile experience part

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import {getProfileById} from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 
 const Profile = ({match, getProfileById, profile: {profile, loading}, auth}) => {
     useEffect(() => {
@@ -30,6 +31,14 @@ const Profile = ({match, getProfileById, profile: {profile, loading}, auth}) => 
             <div className="porfile-grid my-1">
                 <ProfileTop profile={profile}/>
                 <ProfileAbout profile={profile}/>
+                <div className="profile-exp bg-white p-2">
+                    <h2 className="text-primary"></h2>
+                    {profile.experience.length > 0 ? (<Fragment>
+                        {profile.experience.map(experience => (
+                            <ProfileExperience key={experience._id} experience={experience}/>
+                        ))}
+                    </Fragment>) : (<h4>No experience credentials</h4>)}
+                </div>
             </div>
         </Fragment>
     )
