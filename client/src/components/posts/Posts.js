f7287122d7b211f0d08680d0ad922d05aace6db2
Aravind <aravind08222@gmail.com>
2021-05-19 12:48:39 +0530

    Added post item to the posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import {getPosts} from '../../actions/post';
 
 const Posts = ({getPosts, post: {posts, loading}}) => {
@@ -10,9 +11,18 @@ const Posts = ({getPosts, post: {posts, loading}}) => {
         getPosts();
     }, [getPosts]);
 
-    return (
+    return loading ? 
+        <Spinner /> : (
         <Fragment>
-
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Welcome to the community
+            </p>
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post}/>
+                ))}
+            </div>
         </Fragment>
     )
 }
